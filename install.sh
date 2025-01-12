41b3cc29588b4f5e87275d2f76015d0b43bc4bfd
DevDolphin7 <devdolphin7@outlook.com>
2025-01-12 17:21:44 +0000

    Remove virtual environment activation

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 python3 -m venv .virtual-environment
-echo "Local virtual environment created"
-source .virtual-environment/bin/activate
-echo "Local virtual environment activated, installing dependencies:"
+echo "Local virtual environment created, installing dependencies:"
 python3 -m pip install -r dependencies.txt
