2ff95c445dc83efbd94e049ff6b77f1925a2b2ec
DevDolphin7 <devdolphin7@outlook.com>
2025-01-12 15:20:54 +0000

    Testing pip virtual environment for dependencies control

---
+++ app.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+
+class EndpointsJSON ():
+    def __init__(self, root):
+        print("Hello")
+
+EndpointsJSON()
---
+++ install-dependencies.sh
@@ -0,0 +1,3 @@
+python3 -m venv .virtual-environment
+source .virtual-environment/bin/activate
+python3 -m pip install -r dependencies.txt
