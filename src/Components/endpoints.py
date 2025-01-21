9c5cf69b643fd2c3a835986868e2630a6efa771f
DevDolphin7 <devdolphin7@outlook.com>
2025-01-21 14:21:10 +0000

    Get file path from Home component, add type annotations

--- src/Components/endpoints.py
+++ src/Components/endpoints.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
+from typing import Type, Callable
+from src.Fonts.fonts import Fonts
 
 class Endpoints ():
-    def __init__(self, frame, set_content):
-        self.button = ctk.CTkButton(frame, text="In endpoints", command=lambda: set_content("home"))
+    def __init__(self, frame: Type[ctk.CTkFrame], set_content: Callable[[str], None], file_path: str) -> None:
+        self.file_path = file_path
+        self.button = ctk.CTkButton(frame, text=f"In endpoints {file_path}", font=Fonts().body, command=lambda: set_content("home"))
         self.button.pack()
