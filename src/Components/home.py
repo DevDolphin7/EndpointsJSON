4aaf3f0fffb4c54cb8f04062b8f96f0c55a77b03
DevDolphin7 <devdolphin7@outlook.com>
2025-01-21 11:51:01 +0000

    Allow sibling package imports for fonts

--- src/Components/home.py
+++ src/Components/home.py
@@ -1,10 +1,11 @@
 import customtkinter as ctk
+from src.Fonts.fonts import Fonts
 
 class Home ():
     def __init__(self, frame, set_content):
         self.label = ctk.CTkLabel(frame,
                                   text='This app is designed to help you keep an "endpoints.json" file up to date while developing a back-end / server!',
-                                  fg_color="transparent")
+                                  font=Fonts().body)
         self.label.pack()
 
         self.button = ctk.CTkButton(frame, text="In home", command=lambda: set_content("endpoints"))
---
+++ src/Fonts/fonts.py
@@ -0,0 +1,5 @@
+import customtkinter as ctk
+
+class Fonts():
+    def __init__(self):
+        self.body = ctk.CTkFont(family="Helvetica", size=20)
