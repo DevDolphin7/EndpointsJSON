4139b3b94c758a34652c66f23bb84af042868846
DevDolphin7 <devdolphin7@outlook.com>
2025-01-21 14:19:33 +0000

    Add type annotations

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 import customtkinter as ctk
 from Components import Home, Endpoints, Help
 
+ctk.set_default_color_theme("green")
+
 class EndpointsJSON (ctk.CTk):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         
         self.title("Endpoints JSON")
         self.geometry("640x480")
+        self.resizable(width=False, height=False)
         
         self.frame = ctk.CTkFrame(self)
         self.frame.pack(fill="both", padx=20, pady=20)
@@ -16,31 +19,31 @@ class EndpointsJSON (ctk.CTk):
         self.mainloop()
         
     @property
-    def content(self):
+    def content(self) -> str:
         return self._content
     
     @content.setter
-    def content(self, content):
+    def content(self, content: str) -> None:
         valid_content = ["home", "endpoints"]
         if content in valid_content:
             self._content = content
             self.remove_content()
             self.load_content()
             
-    def set_content(self, content):
+    def set_content(self, content: str) -> None:
         self.content = content
         
-    def remove_content(self):
+    def remove_content(self) -> None:
         for widget in self.frame.winfo_children():
             widget.destroy()
 
-    def load_content(self):
+    def load_content(self) -> None:
         match self.content:
             case "home":
                 self.home = Home(self.frame, self.set_content)
             
             case "endpoints":
-                self.endpoints = Endpoints(self.frame, self.set_content)
+                self.endpoints = Endpoints(self.frame, self.set_content, self.home.file)
         
         self.help = Help(self.frame)
 
