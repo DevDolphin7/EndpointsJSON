65ae49dd4515834516438160a8091805d5b4edb5
DevDolphin7 <devdolphin7@outlook.com>
2025-01-22 16:45:15 +0000

    Add save_file, move_endpoint and delete_endpoint functionality

--- src/Components/endpoint.py
+++ src/Components/endpoint.py
@@ -3,20 +3,28 @@ from typing import Type, Callable
 from src.Fonts.fonts import Fonts
 
 class Endpoint ():
-    def __init__(self, frame: Type[ctk.CTkFrame], endpoint: str, data: dict[str, str|list|dict]) -> None:
+    def __init__(self,
+                 frame: Type[ctk.CTkFrame],
+                 endpoint: str, data: dict[str, str|list|dict],
+                 move_endpoint: Callable[[str, str, dict[str, str|list|dict]], None],
+                 delete_endpoint: Callable[[str, dict[str, str|list|dict]], None]):
+        self.full_endpoint = endpoint
         self.method, self.endpoint = endpoint.split(" ")
         self.data = data
+        self.move_endpoint = move_endpoint
+        self.delete_endpoint = delete_endpoint
         
         self.frame = ctk.CTkFrame(frame, corner_radius=10)
         
-        self.tab_view = ctk.CTkTabview(self.frame, height=70, anchor="e")
+        self.tab_view = ctk.CTkTabview(self.frame, height=70, anchor="e", border_width=2, border_color="green")
         self.endpoint_tab = self.tab_view.add("Overview")
         self.detail_tab = self.tab_view.add("Detail")
+        self.set_tab = self.tab_view.add("Set")
         self.tab_view.set("Overview")
         
         self.load_tab_endpoint_info()
         self.load_detail_tab()
-        
+        self.load_set_tab()
         
         self.tab_view.pack(fill="x", padx=5, pady=5)
         self.frame.pack(fill="x", padx=5, pady=5)
@@ -25,14 +33,14 @@ class Endpoint ():
         self.method_menu_var = ctk.StringVar(self.frame, self.method)
         self.endpoint_entry_var = ctk.StringVar(self.frame, self.endpoint)
         
-        for tab in [self.endpoint_tab, self.detail_tab]:
+        for tab in [self.endpoint_tab, self.detail_tab, self.set_tab]:
             frame = ctk.CTkFrame(tab, corner_radius=10)
             
-            self.method_menu = ctk.CTkOptionMenu(frame, values=["GET", "POST", "PATCH", "PUT", "DELETE"], variable=self.method_menu_var)
+            self.method_menu = ctk.CTkOptionMenu(frame, values=["GET", "POST", "PATCH", "PUT", "DELETE"], variable=self.method_menu_var, font=Fonts().body)
             
-            self.endpoint_entry = ctk.CTkEntry(frame, textvariable=self.endpoint_entry_var, width=200)
+            self.endpoint_entry = ctk.CTkEntry(frame, textvariable=self.endpoint_entry_var, width=200, font=Fonts().body)
             
-            if tab.winfo_name() == "!ctkframe":
+            if tab.winfo_name() in ["!ctkframe", "!ctkframe3"]:
                 self.method_menu.configure(state="disabled")
                 self.endpoint_entry.configure(state="disabled")
                 
@@ -52,3 +60,30 @@ class Endpoint ():
             title_entry.pack(side=ctk.LEFT, padx=5)
             info_entry.pack(side=ctk.LEFT, padx=5)
             frame.pack(fill="x", expand=True, ipady=5)
+            
+    def load_set_tab(self) -> None:
+        up_button = ctk.CTkButton(self.set_tab,
+                                  text="🔼",
+                                  font=Fonts().emoji,
+                                  width=50,
+                                  corner_radius=10,
+                                  command=lambda: self.move_endpoint("up", self.full_endpoint, self.data))
+        up_button.pack(side=ctk.RIGHT)
+        
+        down_button = ctk.CTkButton(self.set_tab,
+                                    text="🔽",
+                                    font=Fonts().emoji,
+                                    width=50,
+                                    corner_radius=10,
+                                    command=lambda: self.move_endpoint("down", self.full_endpoint, self.data))
+        down_button.pack(side=ctk.RIGHT, padx=5)
+        
+        delete_button = ctk.CTkButton(self.set_tab,
+                                      text="🗑️",
+                                      font=Fonts().emoji,
+                                      width=50,
+                                      corner_radius=10,
+                                      fg_color="#e8292e",
+                                      hover_color="#b02024",
+                                      command=lambda: self.delete_endpoint(self.full_endpoint, self.data))
+        delete_button.pack(side=ctk.LEFT)
--- src/Components/endpoints.py
+++ src/Components/endpoints.py
@@ -1,7 +1,8 @@
 import customtkinter as ctk
-import json, ast
+import json
 from typing import Type, Callable
 from .endpoint import Endpoint
+from src.Utils.utils import get_all_endpoints
 from src.Fonts.fonts import Fonts
 
 class Endpoints ():
@@ -14,18 +15,33 @@ class Endpoints ():
         self.utility_frame = ctk.CTkFrame(frame, corner_radius=10)        
         self.load_utility_buttons()
         
+        self.file_path_label = ctk.CTkLabel(self.utility_frame, text=f"Path: {self.file_path}", font=Fonts().body)
+        
         self.frame = ctk.CTkScrollableFrame(frame, corner_radius=10)
+        self.frame.bind_all("<Button-4>", lambda e: self.frame._parent_canvas.yview("scroll", -1, "units"))
+        self.frame.bind_all("<Button-5>", lambda e: self.frame._parent_canvas.yview("scroll", 1, "units"))
               
         self.load_endpoints()
         
+        self.file_path_label.pack(side=ctk.LEFT, padx=5)      
         self.utility_frame.pack(fill="x", padx=5, pady=5)    
         self.frame.pack(fill="both", expand=True, padx=5, pady=5)
         
     def load_utility_buttons(self) -> None:        
-        self.home_button = ctk.CTkButton(self.utility_frame, text="🏠", font=Fonts().emoji, width=50, command=lambda: self.set_content("home"))
+        self.home_button = ctk.CTkButton(self.utility_frame,
+                                         text="🏠",
+                                         font=Fonts().emoji,
+                                         width=50,
+                                         corner_radius=10,
+                                         command=lambda: self.set_content("home"))
         self.home_button.pack(side=ctk.LEFT, padx=5)
         
-        self.save_button = ctk.CTkButton(self.utility_frame, text="💾", font=Fonts().emoji, width=50, command=self.save_file)
+        self.save_button = ctk.CTkButton(self.utility_frame,
+                                         text="💾",
+                                         font=Fonts().emoji,
+                                         width=50,
+                                         corner_radius=10,
+                                         command=self.save_file)
         self.save_button.pack(side=ctk.RIGHT, padx=5)
         
     def load_endpoints(self) -> None:
@@ -33,47 +49,57 @@ class Endpoints ():
             self.endpoints: dict = json.load(json_contents)
             json_contents.close()
             
-        print(self.endpoints)
-            
         for endpoint, data in self.endpoints.items():
-            Endpoint(self.frame, endpoint, data)
-            
-    def remove_endpoints(self) -> None:
-        pass
+            Endpoint(self.frame, endpoint, data, self.move_endpoint, self.delete_endpoint)
     
-    def save_file(self) -> None:
-        self.get_data_to_save()
+    def save_file(self, data_to_save: dict[str, str|list|dict]=None) -> None:
+        if data_to_save:
+            self.data_to_save = data_to_save
+        else:
+            self.data_to_save = get_all_endpoints(self.frame)
         
         with open(self.file_path, "w") as endpoints_json:
-            json.dump(self.data_to_save, endpoints_json, indent=4)
+            json.dump(self.data_to_save, endpoints_json, indent=2)
             endpoints_json.close()
+                                    
+    def reload(self) -> None:
+        self.set_content("endpoints")
         
-    def get_data_to_save(self) -> None:
-        self.data_to_save = {}
-        method, current_endpoint, current_info = "", "", ""
+    def get_endpoint_from_user_interaction(self, endpoint: str, data: dict[str, str|list|dict]) -> str:
+        for original_endpoint, original_data in self.endpoints.items():
+            if endpoint == original_endpoint and data == original_data:
+                return endpoint
         
-        for app_widget in self.frame.winfo_children():
-            if isinstance(app_widget, ctk.CTkFrame):
-                tabview: Type[ctk.CTkTabview]
-                for tabview in app_widget.winfo_children():
-                    tabview_frame: Type[ctk.CTkFrame]
-                    for tabview_frame in tabview.tab("Detail").winfo_children():
-                        for widget in tabview_frame.winfo_children():
-                            if isinstance(widget, ctk.CTkOptionMenu):
-                                method = widget.get()
-                            if isinstance(widget, ctk.CTkEntry):
-                                if method != "":
-                                    current_endpoint = f"{method} {widget.get()}"
-                                    self.data_to_save[current_endpoint] = {}
-                                    method, current_info = "", ""
-                                elif current_info == "":
-                                    current_info = widget.get()
-                                    self.data_to_save[current_endpoint][current_info] = None
-                                else:
-                                    data = widget.get()
-                                                                      
-                                    if data[0] in ["{", "[", "("]:
-                                        data = ast.literal_eval(data)
-
-                                    self.data_to_save[current_endpoint][current_info] = data
-                                    current_info = ""
+    def move_endpoint(self, direction: str, endpoint: str, data: dict[str, str|list|dict]) -> None:
+        if direction == "up":
+            index_direction = -1
+        elif direction == "down":
+            index_direction = 1
+        
+        current_endpoint = self.get_endpoint_from_user_interaction(endpoint, data)
+        
+        endpoints: list[str] = list(self.endpoints.keys())
+        current_index = endpoints.index(current_endpoint)
+        new_index = current_index + index_direction
+        
+        if (new_index < 0) or (new_index == len(endpoints)):
+            return
+        
+        all_endoints = get_all_endpoints(self.frame)
+        
+        endpoint_key = endpoints.pop(current_index)
+        endpoints.insert(new_index, endpoint_key)
+        
+        reordered_endoints = {key: all_endoints[key] for key in endpoints}
+        
+        self.save_file(data_to_save=reordered_endoints)
+        self.reload()              
+                
+    def delete_endpoint(self, endpoint: str, data: dict[str, str|list|dict]) -> None:
+        current_endpoint = self.get_endpoint_from_user_interaction(endpoint, data)
+        
+        all_endoints = get_all_endpoints(self.frame)
+        all_endoints.pop(current_endpoint, None)
+        
+        self.save_file(data_to_save=all_endoints)
+        self.reload() 
