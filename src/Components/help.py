cf6ee597c6fe07e095d8f29c2c516dd44182b48b
DevDolphin7 <devdolphin7@outlook.com>
2025-01-22 12:20:25 +0000

    Add light / dark mode functionality

--- src/Components/help.py
+++ src/Components/help.py
@@ -5,14 +5,31 @@ from src.Fonts.fonts import Fonts
 
 class Help ():
     def __init__(self, frame: Type[ctk.CTkFrame]) -> None:
-        self.label = ctk.CTkLabel(frame,
+        self.frame = ctk.CTkFrame(frame, corner_radius=10)
+        
+        self.label = ctk.CTkLabel(self.frame,
                                   text='For help or to get in contact, visit ',
                                   font=Fonts().footer)
         
-        self.hyperlink = ctk.CTkLabel(frame,
+        self.hyperlink = ctk.CTkLabel(self.frame,
                                   text='DevDolphin7 on GitHub',
-                                  font=Fonts().footer)
+                                  font=Fonts().footer_link)
         self.hyperlink.bind("<Button-1>", lambda _: webbrowser.open_new("https://github.com/DevDolphin7/EndpointsJSON"))
         
-        self.label.pack()
-        self.hyperlink.pack()
+        self.light_dark_mode_var = ctk.StringVar(value="light")
+        self.light_dark_mode = ctk.CTkSwitch(self.frame,
+                                             text="🌒 / 🔆",
+                                             font=Fonts().emoji,
+                                             command=self.set_light_dark_mode,
+                                             variable=self.light_dark_mode_var,
+                                             onvalue="light",
+                                             offvalue="dark")
+        
+        self.label.pack(side=ctk.LEFT, padx=10)
+        self.hyperlink.pack(side=ctk.LEFT)
+        self.light_dark_mode.pack(side=ctk.RIGHT, padx=10)
+        
+        self.frame.pack(side=ctk.BOTTOM, fill="x")
+        
+    def set_light_dark_mode(self) -> None:
+        ctk.set_appearance_mode(self.light_dark_mode_var.get())
