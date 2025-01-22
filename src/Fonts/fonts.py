a6599aa77f60dbaa66f97a83d76ae96d53ef504b
DevDolphin7 <devdolphin7@outlook.com>
2025-01-22 12:19:23 +0000

    Add locally saved emoji font and link font

--- src/Fonts/fonts.py
+++ src/Fonts/fonts.py
@@ -1,7 +1,13 @@
 import customtkinter as ctk
+import os
+
+emoji_font_path = os.path.join(os.path.dirname(__file__), "Noto_Emoji/NotoEmoji-VariableFont_wght.ttf")
+ctk.FontManager.load_font(emoji_font_path)
 
 class Fonts():
     def __init__(self):
         self.header = ctk.CTkFont(family="Helvetica", size=20)
+        self.emoji = ctk.CTkFont(family="NotoEmoji-VariableFont_wght", size=20)
         self.body = ctk.CTkFont(family="Helvetica", size=15)
         self.footer = ctk.CTkFont(family="Helvetica", size=10)
+        self.footer_link = ctk.CTkFont(family="Helvetica", size=10, underline=True, weight="bold")
