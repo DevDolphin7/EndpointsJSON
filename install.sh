2d8f983dd9686bc31d865eb1307803d76a8ab15d
DevDolphin7 <devdolphin7@outlook.com>
2025-01-23 14:36:32 +0000

    Locally install src for sibling imports

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ echo "Local virtual environment created"
 source .virtual-environment/bin/activate
 echo "Local virtual environment activated, installing dependencies:"
 python3 -m pip install -r dependencies.txt
+echo "dependencies installed, installing this module in an editable state for sibling imports, see https://stackoverflow.com/questions/6323860/sibling-package-imports"
+python3 -m pip install -e .
